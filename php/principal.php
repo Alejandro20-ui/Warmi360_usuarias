d492482a68d40dee784bb691a36902f0bb57f91d
Alejandro Manuel <alejandro.dlcescate@gmail.com>
2025-11-27 22:10:40 -0500

    mejora 2.1

--- php/principal.php
+++ php/principal.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['user_id'])) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WARMI360 - Inicio</title>
     <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
